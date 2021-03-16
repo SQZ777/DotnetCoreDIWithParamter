ef9362676f4b0cef55984d823f1e4763951e4f26
sqz777 <sqz777isme@gmail.com>
2021-03-17 00:09:05 +0800

    add example code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace DotnetCoreDIWithParameter
 {
@@ -6,7 +7,47 @@ namespace DotnetCoreDIWithParameter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddScoped<IAction, Action>();
+            serviceCollection.AddScoped<IDog>(svc => new Dog(svc.GetService<IAction>()));
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var dog = serviceProvider.GetService<IDog>();
+
+            dog.CallShakeHands();
+        }
+
+        public interface IAction
+        {
+            void ShakeHands();
+        }
+
+        public class Action : IAction
+        {
+            public void ShakeHands()
+            {
+                Console.WriteLine("Shaking hands!");
+            }
+        }
+
+        public interface IDog
+        {
+            void CallShakeHands();
+        }
+
+        public class Dog : IDog
+        {
+            IAction _action;
+            public Dog(IAction action)
+            {
+                _action = action;
+            }
+            public void CallShakeHands()
+            {
+                Console.WriteLine("Dog: ");
+                _action.ShakeHands();
+            }
         }
     }
 }
